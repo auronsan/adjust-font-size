7d794c9b6daf2ab8bac1b4ca446eae61ecbded7e
Ahsanul <auronsanjr@gmail.com>
2016-09-27 15:20:43 +0700

    Update adjustmodule.java

--- adjustmodule.java
+++ adjustmodule.java
@@ -1,21 +1,24 @@
 public class adjustmodule {
+            private WebView mWebView;
+            private int fontSize;
+            
             public setFontSize(View rootView,int value){ 
-                        WebView mWebView = (WebView) rootView.findViewById(R.id.webview);
-                        fontSize = mWebView.getSettings().getDefaultFontSize(); 
+                        mWebView = (WebView) rootView.findViewById(R.id.webview);                    
                         mWebView.getSettings().setDefaultFontSize(value);
+                        return rootView;
             }
-            private void fontSizePlus() {
+            private void fontSizePlus(View rootView) {
+                        mWebView = (WebView) rootView.findViewById(R.id.webview);  
+                        fontSize = mWebView.getSettings().getDefaultFontSize();
                         fontSize++;
                         this.changeFontSize(fontSize);
             }
 
-            private void fontSizeMinus() {
-            fontSize--;
-            this.changeFontSize(fontSize);
-            }
-
-            private void changeFontSize(int value) {
-            mWebView.getSettings().setDefaultFontSize(value);
+            private void fontSizeMinus(View rootView) {
+                        mWebView = (WebView) rootView.findViewById(R.id.webview);  
+                        fontSize = mWebView.getSettings().getDefaultFontSize();
+                        fontSize--;
+                        this.changeFontSize(fontSize);
             }
 
 }
