e61946d4cca520a111f4beb21d1f8fd5587c4426
Ahsanul <auronsanjr@gmail.com>
2016-09-27 13:57:59 +0700

    Update and rename test.java to testFragment.java

---
+++ testFragment.java
@@ -0,0 +1,27 @@
+package com.example.anshor.adjust-font-size.app;
+
+public class testFragment extends Fragment {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        mWebView = (WebView) rootView.findViewById(R.id.webview);
+        fontSize = mWebView.getSettings().getDefaultFontSize();
+        
+        return rootView;
+        }
+        private void fontSizePlus() {
+            fontSize++;
+            this.changeFontSize(fontSize);
+        }
+
+        private void fontSizeMinus() {
+            fontSize--;
+            this.changeFontSize(fontSize);
+        }
+
+        private void changeFontSize(int value) {
+            mWebView.getSettings().setDefaultFontSize(value);
+        }
+
+}
