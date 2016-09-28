d4f5a1682fecbbaff1668adbfafa7de6fd2bd459
Ahsanul <auronsanjr@gmail.com>
2016-09-28 08:02:33 +0700

    Update adjustmodule.java

--- adjustmodule.java
+++ adjustmodule.java
@@ -1,6 +1,9 @@
 public class adjustmodule {
             private static WebView mWebView;
             private static int fontSize;
+            private int mMaxLines;
+            private float mMinTextSize;
+            private float mMaxTextSize;
             
             private static void changeFontSize(int value) {
                         mWebView.getSettings().setDefaultFontSize(value);
@@ -17,48 +20,54 @@ public class adjustmodule {
             private static void autofit(TextView view, TextPaint paint, float minTextSize, float maxTextSize,
                 int maxLines){
                         if (maxLines <= 0 || maxLines == Integer.MAX_VALUE) {
-            return;
-            }
+                                    return;
+                        }
 
-            int targetWidth = view.getWidth() - view.getPaddingLeft() - view.getPaddingRight();
-            if (targetWidth <= 0) {
-                        return;
-            }
+                        int targetWidth = view.getWidth() - view.getPaddingLeft() - view.getPaddingRight();
+                        if (targetWidth <= 0) {
+                                    return;
+                        }
 
-            CharSequence text = view.getText();
-            TransformationMethod method = view.getTransformationMethod();
-            if (method != null) {
-                        text = method.getTransformation(text, view);
-            }
+                        CharSequence text = view.getText();
+                        TransformationMethod method = view.getTransformationMethod();
+                        if (method != null) {
+                                    text = method.getTransformation(text, view);
+                        }
 
-            Context context = view.getContext();
-            Resources r = Resources.getSystem();
-            DisplayMetrics displayMetrics;
+                        Context context = view.getContext();
+                        Resources r = Resources.getSystem();
+                        DisplayMetrics displayMetrics;
 
-            float size = maxTextSize;
-            float high = size;
-            float low = 0;
+                        float size = maxTextSize;
+                        float high = size;
+                        float low = 0;
 
-            if (context != null) {
-                        r = context.getResources();
-            }
-            displayMetrics = r.getDisplayMetrics();
+                        if (context != null) {
+                                    r = context.getResources();
+                        }
+                        displayMetrics = r.getDisplayMetrics();
 
-            paint.set(view.getPaint());
-            paint.setTextSize(size);
+                        paint.set(view.getPaint());
+                        paint.setTextSize(size);
 
-            if ((maxLines == 1 && paint.measureText(text, 0, text.length()) > targetWidth)
-                || getLineCount(text, paint, size, targetWidth, displayMetrics) > maxLines) {
+                        if ((maxLines == 1 && paint.measureText(text, 0, text.length()) > targetWidth)
+                            || getLineCount(text, paint, size, targetWidth, displayMetrics) > maxLines) {
                                       size = getAutofitTextSize(text, paint, targetWidth, maxLines, low, high,
                                       displayMetrics);
                                     }
 
-            if (size < minTextSize) {
-                        size = minTextSize;
-            }
+                        if (size < minTextSize) {
+                                    size = minTextSize;
+                        }
 
-            view.setTextSize(TypedValue.COMPLEX_UNIT_PX, size);
-            }
+                        view.setTextSize(TypedValue.COMPLEX_UNIT_PX, size);
+                        }
             public float getMinTextSize() {
                         return mMinTextSize;
              }
+            public float getMaxTextSize() {
+                        return mMaxTextSize;
+            }
+            public int getMaxLines() {
+               return mMaxLines;
+            }
